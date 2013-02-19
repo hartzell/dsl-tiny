77cd213d4740d02b16d6c7b55b46d41840e18176
George Hartzell <hartzell@alerce.com>
2013-02-18 17:44:56 -0800

    * add pod

--- lib/DSL/Tiny/Role.pm
+++ lib/DSL/Tiny/Role.pm
@@ -1,7 +1,81 @@
 package DSL::Tiny::Role;
 
+# ABSTRACT: Import a DSL into a package.
 
+=head1 SYNOPSIS
 
+    # in e.g. MyDSL.pm
+    # put together class with a simple dsl
+    package MyDSL;
+    
+    use Moo;
+    
+    with qw(DSL::Tiny::Role);
+    
+    use MyDelegate;
+    use Sub::Exporter::Util qw(curry_chain curry_method);
+    
+    sub build_dsl_keywords {
+        return [
+            qw(callme incr),
+            break_encapsulation => { as => curry_method('return_self'), },
+            beep                => {
+                as    => curry_chain( delegate => 'beep' ),
+                after => curry_method('do_it_after'),
+            },
+            value => { before => curry_method('do_it_before'), },
+        ];
+    }
+    
+    has before_counter => ( is => 'rw', default => sub {0}, );
+    has counter        => ( is => 'rw', default => sub {0}, );
+    has after_counter  => ( is => 'rw', default => sub {0}, );
+    
+    sub do_it_before {
+        my $self = shift;
+        my $i    = $self->before_counter() + 1;
+        $self->before_counter($i);
+    }
+    
+    sub do_it_after {
+        my $self = shift;
+        my $i    = $self->after_counter() + 1;
+        $self->after_counter($i);
+    }
+    
+    sub callme { return "sometime..." }
+    
+    sub return_self { return $_[0] }
+    
+    sub delegate { return MyDelegate->new() }
+    
+    sub incr { $_[0]->counter( $_[0]->counter() + 1 ) }
+    
+    sub value { $_[0]->counter }
+    
+    1;
+    
+    ################################################################
+
+    # and then in another package
+
+    use MyDSL;
+
+    use Test::More;
+
+    is(value, 0, "Got the correct value");
+    incr;
+    incr;
+    is(value, 2, "Got the correct value");
+
+    is(callme, "sometime...", "Got the right response");
+
+    # etc....
+    done_testing;
+
+=head1 DESCRIPTION
+
+=cut
 
 use Moo::Role;
 
